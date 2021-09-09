04624b968ec848613dadb9f31203cab5bdfc4a81
mwanginjamb <francnjamb@gmail.com>
2021-09-09 20:13:39 +0300

    Feat Add a datetime-local field: - Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="onClick()" :style="{ background: color}">{{title}}</button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            title: String,
+            color: String,
+        },
+        methods: {
+            onClick() {
+                console.log('You just clicked me....');
+            }
+        }
+    }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button class="btn" color="green" title="Add Task" />
+    </header>
+    
+</template>
+
+
+
+
+<script>
+import Button from './Button.vue'
+export default {
+   
+    name: 'Header',
+    components: {
+        Button
+    },
+    props: {
+        title: String
+    }
+    
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
